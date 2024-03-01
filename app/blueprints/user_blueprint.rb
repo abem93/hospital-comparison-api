7ea0b6ffe39034d06e4afeeddc7d850dacb31dfb
abem93 <aimejia4@gmail.com>
2024-02-29 18:42:27 -0600

    user controller and blueprint added

---
+++ app/blueprints/user_blueprint.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class UserBlueprint < Blueprinter::Base
+  identifier :id
+
+  view :me do
+    fields  :username, :email, :zipcode
+  end
+  view :normal do
+    fields :username
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :zipcode, length: { is: 5 }, 
-
 end
