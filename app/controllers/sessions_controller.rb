66e8053b0e57922eda3b3318c6ad5ebcc4cfbe13
abem93 <aimejia4@gmail.com>
2024-03-14 11:40:18 -0500

    CONFIG(deployment)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params['email'])
-    if user.authenticate(params['password'])
+    if user&.authenticate(params['password'])
       token = jwt_encode(user_id: user.id)
-      render json: { token: token }, status: :ok
+      render json: { token: token , status: :ok}
     else
-      render json: { error: 'Unauthorized' }, status: :unauthorized
+      render json: { error: 'Unauthorized', status: :unauthorized}
     end
   end
 
