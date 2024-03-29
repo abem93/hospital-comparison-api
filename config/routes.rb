1091a410095d60be8f6aa2afb7a3d0867a7f725a
abem93 <aimejia4@gmail.com>
2024-03-28 19:07:10 -0500

    added web controller and updated db data

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
- scope '/' do
-  post 'login', to: 'sessions#create'
- end
+  get 'web/bootstrap'
+  scope '/' do
+    post 'login', to: 'sessions#create'
+  end
  
- resources :users
- resources :procedures
- resources :procedure_costs
- resources :insurances
- resources :hospitals
+  resources :users
+  resources :procedures
+  resources :procedure_costs
+  resources :insurances
+  resources :hospitals
 end
